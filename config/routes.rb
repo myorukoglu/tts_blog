2cb85edf0eb661476c85f50f6ebaf007025cc26d
Murat Yorukoglu <murat.yorukoglu@gmail.com>
2019-11-25 10:22:20 -0500

    basic bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+
+
+  
+  get 'index' => 'practice#index'
+  
+  post 'index' => 'practice#index'
+
+  get 'about' => 'practice#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
