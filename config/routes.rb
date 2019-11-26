b404f04944445bcaa45fbc3d00dbc3075c07443d
Murat Yorukoglu <murat.yorukoglu@gmail.com>
2019-11-26 10:09:39 -0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   resources :posts
 
 
+  root 'practice#index'
   
-  get 'index' => 'practice#index'
+  get 'home' => 'practice#index'
   
-  post 'index' => 'practice#index'
+  post 'home' => 'practice#index'
 
   get 'about' => 'practice#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
